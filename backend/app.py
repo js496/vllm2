345ad5b9145f9c08e732f46dca52d8b0b64e6e75
js496 <o0xqa9@gmail.com>
2025-02-13 05:36:22 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,12 @@ logger = logging.getLogger(__name__)
 # Global variables
 device_request = DeviceRequest(count=-1, capabilities=[["gpu"]])
 client = docker.from_env()
-r = redis.Redis(host="redis", port=6379, db=0)
+
+REDIS_IP = "123.217.93.26"
+REDIS_PORT = 59508
+r = redis.Redis(host=REDIS_IP, port=REDIS_PORT, db=0)
+
+
 
 
 def get_gpu_info() -> List[Dict[str, float]]:
